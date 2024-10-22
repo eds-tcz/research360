c95a46f4e8518e6fcd5100948d2a92bf9dfbe230
eds-tcz <edstechchefz@gmail.com>
2024-10-22 15:49:14 +0530

    Update market-sentiments.js

--- blocks/market-sentiments/market-sentiments.js
+++ blocks/market-sentiments/market-sentiments.js
@@ -1,6 +1,6 @@
-// export default function decorate(block)
-// {
-//     [...block.children].forEach((row, r) => {
-//         row.classList.add('market-sentiments-text');
-//     });
-// }
+export default function decorate(block)
+{
+    [...block.children].forEach((row, r) => {
+        row.classList.add('market-sentiments-text');
+    });
+}
