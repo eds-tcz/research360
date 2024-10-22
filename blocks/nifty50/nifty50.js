5cf7dcc72e5430028ba7b1d227ec72df80c98aed
pankajTCZ <pankaj.datt@techchefz.com>
2024-10-22 14:05:20 +0530

    fix

--- blocks/nifty50/nifty50.js
+++ blocks/nifty50/nifty50.js
@@ -1,6 +1,8 @@
-export default function decorate(block)
-{
+export default function decorate(block) {
     [...block.children].forEach((row, r) => {
         row.classList.add('nifty-cards');
+        [...row.children].forEach((div, index) => {
+            div.classList.add(`nifty-cards-${index + 1}`);
+        });
     });
 }
