130725d445d0f26c544a55198fe7630ba0bfcdce
pankajTCZ <pankaj.datt@techchefz.com>
2024-10-22 14:11:20 +0530

    js fix

--- blocks/nifty50/nifty50.js
+++ blocks/nifty50/nifty50.js
@@ -4,5 +4,18 @@ export default function decorate(block) {
         [...row.children].forEach((div, index) => {
             div.classList.add(`nifty-cards-${index + 1}`);
         });
+        const cardContents = {
+            0: ['NIFTY 50', '19,789.35', '-135.20', '-0.68%'],
+            1: ['BANK NIFTY', '44,521.45', '-289.65', '-0.72%'],
+            2: ['NIFTY IT', '33,256.80', '+156.40', '+0.45%']
+        };
+
+        div.innerHTML = '';
+        
+        cardContents[index].forEach(content => {
+            const p = document.createElement('p');
+            p.textContent = content;
+            div.appendChild(p);
+        });
     });
 }
