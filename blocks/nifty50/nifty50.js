055f52cf65315aa2d3553c015e7c48942a60c44e
pankajTCZ <pankaj.datt@techchefz.com>
2024-10-22 14:14:23 +0530

    p elements fix

--- blocks/nifty50/nifty50.js
+++ blocks/nifty50/nifty50.js
@@ -11,6 +11,20 @@ export default function decorate(block) {
                 <p>-135.20</p>
                 <p>-0.68%</p>
             `;
+        } else if (index === 1) {
+            div.innerHTML = `
+                <p>BANK NIFTY</p>
+                <p>44,521.45</p>
+                <p>-289.65</p>
+                <p>-0.72%</p>
+            `;
+        } else if (index === 2) {
+            div.innerHTML = `
+                <p>NIFTY IT</p>
+                <p>33,256.80</p>
+                <p>+156.40</p>
+                <p>+0.45%</p>
+            `;
         }
     });
 }
