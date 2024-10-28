3fe90a5bcb99e85a4b00b950db53caf423cbe59f
pankajTCZ <pankaj.datt@techchefz.com>
2024-10-28 14:32:38 +0530

    World-indices Block

---
+++ blocks/world-indices/world-indices.js
@@ -0,0 +1,16 @@
+export default function decorate(block) {
+    [...block.children].forEach((row, r) => {
+        row.classList.add('world-indices-div');
+        [...row.children].forEach((div, index) => {
+            div.classList.add(`world-indices-${index + 1}`);
+            const paragraphs = div.querySelectorAll('p');
+            paragraphs.forEach((p, pIndex) => {
+                p.classList.add(`world-indices-text-${pIndex + 1}`);
+            });
+            if (index === 0 && r === 0) {
+                const hr = document.createElement('hr');
+                // div.parentNode.insertBefore(hr, div.nextSibling);
+            }
+        });
+    });
+}
